c0e3453bc09681e18fc4ac21ed871823ad9465fb
marianaeslan <mariana.eslan@gmail.com>
2025-01-22 11:04:52 -0500

    feat: add new conversions

--- index.js
+++ index.js
@@ -3,21 +3,36 @@ const inputEl = document.getElementById("input-el")
 let resultLength = document.getElementById("len-result")
 let resultVol= document.getElementById("vol-result")
 let resultMass = document.getElementById("mass-result")
+let resultTemp = document.getElementById("temp-result")
+let resultL = document.getElementById("l-result")
+let resultDist = document.getElementById("dist-result")
 const hiddenSpan = document.getElementById('hidden-span')
 
 converterBtn.addEventListener("click", function() {
     let baseValue = inputEl.value
-    let toFeet = (baseValue * 3.281).toFixed(3)
-    let toMeters = (baseValue / 3.281).toFixed(3)
+    let toFeet = (baseValue * 3.281).toFixed(2)
+    let toMeters = (baseValue / 3.281).toFixed(2)
     resultLength.textContent = `${baseValue} meters = ${toFeet} feet | ${baseValue} feet = ${toMeters} meters`
 
-    let toGallons = (baseValue * 0.264).toFixed(3)
-    let toLiters = (baseValue / 0.264).toFixed(3)
+    let toCm = baseValue / 100
+    let toM = baseValue * 100
+    resultL.textContent = `${baseValue} meters = ${toCm} cm | ${baseValue} cm = ${toM} meters`
+
+    let toGallons = (baseValue * 0.264).toFixed(1)
+    let toLiters = (baseValue / 0.264).toFixed(1)
     resultVol.textContent = `${baseValue} liters = ${toGallons} gallons | ${baseValue} gallons = ${toLiters} liters`
 
-    let toPounds = (baseValue * 2.204).toFixed(3)
-    let toKg = (baseValue / 2.204).toFixed(3)
+    let toPounds = (baseValue * 2.204).toFixed(2)
+    let toKg = (baseValue / 2.204).toFixed(2)
     resultMass.textContent = `${baseValue} kilos = ${toPounds} pounds | ${baseValue} pounds = ${toKg} kilos`
+
+    let toCelsius = ((baseValue - 32) / 1.8).toFixed(1)
+    let toFarenheit = ((baseValue * 1.8) + 32).toFixed(1)
+    resultTemp.textContent = `${baseValue} fahrenheit = ${toCelsius} celsius | ${baseValue} celsius = ${toFarenheit} fahrenheit`
+
+    let toKm = (baseValue * 1.6).toFixed(1)
+    let toMiles = (baseValue / 1.6).toFixed(1)
+    resultDist.textContent = `${baseValue} miles = ${toKm} Km | ${baseValue} Km = ${toMiles} miles`
 })
 
 inputEl.addEventListener("keydown", function(event) {
